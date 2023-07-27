3ca593e9d28096e193dd6a426e12a20ba4aa81d8
Илий Дарья <daria.iliy21@gmail.com>
2023-07-27 22:50:14 +0300

    Исправила ошибку обновления

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -138,16 +138,15 @@ class RecipePostSerializer(serializers.ModelSerializer):
         instance.text = validated_data.get('text', instance.text)
         instance.cooking_time = validated_data.get(
             'cooking_time', instance.cooking_time)
-        ingredients = validated_data.pop('ingredients', None)
-        if ingredients is not None:
-            RecipeIngredient.objects.filter(recipe=instance).delete()
-            RecipeIngredient.objects.bulk_create([
-                RecipeIngredient(
-                    ingredient_id=ingredient['id'],
-                    recipe=instance,
-                    amount=ingredient['amount']
-                ) for ingredient in ingredients
-            ])
+        ingredients = validated_data.pop('ingredients')
+        instance.recipeingredient_set.all().delete()
+        for ingredient in ingredients:
+            ingredient = Ingredient.objects.get(pk=ingredient['id'])
+            RecipeIngredient.objects.create(
+                ingredient=ingredient,
+                recipe=instance,
+                amount=ingredient['amount']
+            )
         instance.save()
         return instance
 
